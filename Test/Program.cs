d8c636dc00a37201e53e8cec2ec85bc5d6118b40
Maksio <jozef@gorowo.pl>
2022-09-20 14:58:34 +0200

    Basic Commit 1
    2 private constructors
    1 public constructor
    private int To Bool; bool To int; Delete zeros;
    override ToString

---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using VLN;
+
+namespace Test
+{
+    class Program
+    {
+        public static void Wypisz(sbyte a)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write(a&1);
+                a = (sbyte)(a >> 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            V_Long v = new(14);
+            Console.WriteLine(v);
+        }
+    }
+}
---
+++ VLN/VLN.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace VLN
+{
+    public struct V_Long
+    {
+        private bool[] number;
+
+        public V_Long() => number = new bool[0];
+        private V_Long(bool[] number)
+        {
+            this.number = number;
+        }
+        public V_Long(sbyte Number)
+        {
+            number = new bool[8];
+            if (Number < 0)
+            {
+                number[0] = true;
+            }
+            else
+                number[0] = false;
+            for (int i = 1; i < 8; i++)
+            {
+                number[i] = iTb(Number & 1);
+                Number >>= 1;
+            }
+            usunZera();
+        }
+
+        private bool iTb(int a)
+        {
+            if (a == 1)
+                return true;
+            else if (a == 0)
+                return false;
+            throw new ArgumentException("Wartość konwertowana na bool nie jest binarna");
+        }
+        private int bTi(bool a) => a ? 1 : 0;
+        private void usunZera()
+        {
+            int index = 0;
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                if (number[i])
+                {
+                    index = i;
+                    break;
+                }
+            }
+            bool[] New = new bool[index + 1];
+            for (int i = 0; i < New.Length; i++)
+            {
+                New[i] = number[i];
+            }
+            number = New;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (bool b in number)
+                sb.Append(bTi(b));
+            return sb.ToString();
+        }
+    }
+}
